20714da0d63a30c8026c8a9d6a96b90d007c89a3
vl013 bc2025-3 <vladfedaschuk2007@gmail.com>
2025-10-21 17:37:22 +0300

    First part of lab 3 (Commander basic version)

--- main.js
+++ main.js
@@ -3,47 +3,33 @@ program
 .requiredOption('-i, --input <file>', 'Input file JSON') 
 .option('-o, --output<file>', 'Way to file where save results') 
 .option('-d, --display', 'Output in console') 
-.option('-f, --furnished', 'Furnished house') 
-.option('-p, --price <number>', 'price less then:',parseInt); 
- 
+.option('-f, -furnished', 'Furnished house') 
+.option('-p, -price <number>', 'price less then:',parseInt); 
 program.parse(process.argv); 
 const options = program.opts(); 
 console.log('Option:',options); 
 const fs = require('fs'); 
-
 if(!options.input){ 
-  console.error('Please,specify input file'); 
-  process.exit(1); 
+console.error('Please,specify input file'); 
+process.exit(1); 
 } 
-
 if(!fs.existsSync(options.input)) { 
-  console.error('Cannot find input file'); 
-  process.exit(1); 
+console.error('Cannot find input file'); 
+process.exit(1); 
 } 
-
 if(!options.output &&!options.display&&!options.furnished&&!options.price){ 
-  process.exit(0); 
+process.exit(0); 
 } 
-
-const rawData = fs.readFileSync(options.input, 'utf8'); 
-let houses = JSON.parse(rawData); // читаємо масив об'єктів 
-let filteredHouses = houses; 
-
-if (options.furnished) { 
-  filteredHouses = filteredHouses.filter(h => h.furnishingstatus === 'furnished'); 
-} 
-
-if (options.price) { 
-  filteredHouses = filteredHouses.filter(h => h.price < options.price); 
-} 
-
+const simulatedResult = [ 
+'13300000 7420', 
+'12250000 8960' 
+]; 
+// Вивід у консоль 
 if (options.display) { 
-  filteredHouses.forEach(house => { 
-    console.log(`${house.price} ${house.area}`); 
-  }); 
+simulatedResult.forEach(line => console.log(line)); 
 } 
-
+// Запис у файл 
 if (options.output) { 
-  const outputData = filteredHouses.map(h => `${h.price} ${h.area}`).join('\n'); 
-  fs.writeFileSync(options.output, outputData, 'utf8'); 
-} 
+const outputData = simulatedResult.join('\n'); 
+fs.writeFileSync(options.output, outputData, 'utf8'); 
+}
