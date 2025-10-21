36085f9321da218f93b9e85b88f2ccc8dd64aeec
vl013 <vladfedaschuk2007@gmail.com>
2025-10-21 17:43:43 +0300

    main.js

--- main.js
+++ main.js
@@ -1,35 +1,59 @@
-const {program}=require('commander'); 
-program  
-.requiredOption('-i, --input <file>', 'Input file JSON') 
-.option('-o, --output<file>', 'Way to file where save results') 
-.option('-d, --display', 'Output in console') 
-.option('-f, -furnished', 'Furnished house') 
-.option('-p, -price <number>', 'price less then:',parseInt); 
-program.parse(process.argv); 
-const options = program.opts(); 
-console.log('Option:',options); 
-const fs = require('fs'); 
-if(!options.input){ 
-console.error('Please,specify input file'); 
-process.exit(1); 
-} 
-if(!fs.existsSync(options.input)) { 
-console.error('Cannot find input file'); 
-process.exit(1); 
-} 
-if(!options.output &&!options.display&&!options.furnished&&!options.price){ 
-process.exit(0); 
-} 
-const simulatedResult = [ 
-'13300000 7420', 
-'12250000 8960' 
-]; 
-// Вивід у консоль 
-if (options.display) { 
-simulatedResult.forEach(line => console.log(line)); 
-} 
-// Запис у файл 
-if (options.output) { 
-const outputData = simulatedResult.join('\n'); 
-fs.writeFileSync(options.output, outputData, 'utf8'); 
+const { program } = require('commander');
+const fs = require('fs');
+
+program
+  .requiredOption('-i, --input <file>', 'Input JSON file')
+  .option('-o, --output <file>', 'Output file path')
+  .option('-d, --display', 'Display result in console')
+  .option('-f, --furnished', 'Show only furnished houses')
+  .option('-p, --price <number>', 'Show only houses with price less than given', parseFloat);
+
+program.parse(process.argv);
+const options = program.opts();
+
+
+if (!options.input) {
+  console.error('Please, specify input file');
+  process.exit(1);
+}
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+if (!options.output && !options.display && !options.furnished && !options.price) {
+  process.exit(0);
+}
+
+
+const rawData = fs.readFileSync(options.input, 'utf8');
+let houses = JSON.parse(rawData);
+
+
+let filtered = houses;
+
+if (options.furnished) {
+  filtered = filtered.filter(h => {
+    const value = (h.furnishingstatus || '').toLowerCase();
+    return value.includes('furnished');
+  });
+}
+
+if (options.price) {
+  filtered = filtered.filter(h => h.price < options.price);
+}
+
+
+if (options.display) {
+  if (filtered.length === 0) {
+    console.log('No matching records found');
+  } else {
+    filtered.forEach(h => console.log(`${h.price} ${h.area}`));
+  }
+}
+
+
+if (options.output) {
+  const outputData = filtered.map(h => `${h.price} ${h.area}`).join('\n');
+  fs.writeFileSync(options.output, outputData, 'utf8');
+  console.log(`Результат записано у файл ${options.output}`);
 }
